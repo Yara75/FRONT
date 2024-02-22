7d91d52031ce717e430d37a0e9675a770cc0bdfb
yureiiko <101016323+yureiiko@users.noreply.github.com>
2024-02-22 15:13:55 +0100

    First commit in back branch.

--- src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
+++ src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackSkeletonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackSkeletonApplication.class, args);
 	}
