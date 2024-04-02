b96111a1720b07722233a9f55de4c84ffb6f1844
yureiiko <101016323+yureiiko@users.noreply.github.com>
2024-04-02 14:03:06 +0200

    Change od PersonalInfo.java

--- src/main/java/com/takima/backskeleton/models/PersonalInfo.java
+++ src/main/java/com/takima/backskeleton/models/PersonalInfo.java
@@ -1,62 +1,28 @@
 package com.takima.backskeleton.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Builder;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Table(name = "PersonalInfo")
+
+@Setter
 @Getter
+@Entity
+@Table(name = "personalinfo")
 public class PersonalInfo {
-    @Column(name="item")
-    private String item;
-    @Column(name = "info")
-    private String info;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public PersonalInfo(Long id, String item, String info) {
-        this.id=id; this.item=item; this.info=info;
-    }
-
-    private PersonalInfo(Builder builder) {
-        this.id=builder.getId();
-        this.item=builder.getItem();
-        this.info=builder.getInfo();
-    }
+    private String nom;
+    private String prenom;
 
     public PersonalInfo() {
-
+        // Constructeur par défaut
     }
 
-    @Getter
-    public static class Builder {
-        private Long id;
-        private String item;
-        private String info;
-
-        public Builder(Long id, String item, String info) {
-            this.id = id; this.item=item; this.info=info;
-        }
-
-        public Builder id(Long id) {
-            this.id=id; return this;
-        }
-
-        public Builder item(String item) {
-            this.item=item; return this;
-        }
-
-        public Builder info(String info) {
-            this.info=info; return this;
-        }
-
-        public PersonalInfo build() {
-            return new PersonalInfo(this);
-        }
+    public PersonalInfo(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
     }
+
 }
