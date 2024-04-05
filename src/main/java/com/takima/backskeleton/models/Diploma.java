102a502ccb7de0f65c838c5985789761ccded74a
yureiiko <101016323+yureiiko@users.noreply.github.com>
2024-04-05 08:27:48 +0200

    Add generated value IDENTITY or all IDs .

--- src/main/java/com/takima/backskeleton/models/Diploma.java
+++ src/main/java/com/takima/backskeleton/models/Diploma.java
@@ -1,9 +1,6 @@
 package com.takima.backskeleton.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +12,7 @@ import java.sql.Date;
 @Table(name="Diploma")
 public class Diploma {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name="obtentiondate")
     private Date obtentionDate;
